33ce7f252e4cd49d39edd98bf48b2b9bdddd56d3
Serena Fernandes <serenafribeiro@gmail.com>
2016-05-12 13:55:43 -0300

    Adds users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508145851) do
+ActiveRecord::Schema.define(version: 20160512165457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,5 +35,13 @@ ActiveRecord::Schema.define(version: 20160508145851) do
   end
 
   add_index "posts", ["author", "created_at"], name: "index_posts_on_author_and_created_at", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
