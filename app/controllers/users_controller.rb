84dd7e91aa800854aaa5567dd435bb149f12ebdd
Serena Fernandes <serenafribeiro@gmail.com>
2016-05-13 15:57:03 -0300

    Fix users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        flash[:danger] = "Usuario criado ${@user.name} com sucesso"
+        format.html { redirect_to users_path }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -71,7 +72,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
     # Confirms the correct user.
