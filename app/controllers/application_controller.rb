5156ec94f783b207b3ca0eea96ac75744e528fd4
Serena Fernandes <serenafribeiro@gmail.com>
2016-05-13 12:09:53 -0300

    Adds set locale method to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
 
   private
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
 
     # Confirms a logged-in user.
     def logged_in_user
@@ -14,4 +20,5 @@ class ApplicationController < ActionController::Base
         render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
       end
     end
+
 end
