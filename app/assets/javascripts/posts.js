685c5f2a93b96bcac9ce57f6b5262e1e62a4894d
Serena Fernandes <serenafribeiro@gmail.com>
2016-05-13 11:51:59 -0300

    Adds summer note to vendor

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,33 @@
+var summernoteConfig = {
+    lang: document.documentElement.lang,
+    fontNames: ['Lato', 'Roboto', 'Arial', 'Helvetica', 'sans-serif', 'monospace', 'serif', 'PT Serif' ],
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'italic', 'underline', 'clear']],
+        ['fontname', ['fontname']],
+        ['fontsize', ['fontsize']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']]
+      ],
+    placeholder: 'Escreva aqui o conteúdo desta publicação'
+}
+
+
+$(document).ready(function(){
+    $('#post-content-editor').append($('#post_content').val())
+    $('#post-content-editor').summernote(summernoteConfig);
+
+    $('#blog-edit').click(function() {
+        summernoteConfig['focus'] = true;
+        $('#post-content-editor').summernote(summernoteConfig);
+    });
+
+    $('#blog-preview').click(function() {
+        var aHTML = $('#post-content-editor').summernote('code');
+        $('#post_content').val(aHTML);
+        $('#post-content-editor').summernote('destroy');
+    });
+});
